7adfe95d0487dcb47c0225dc10aecbe0c5ce3a5d
WskyMike <originid1488@ya.ru>
2025-05-23 00:18:25 +0300

    bugfix: changed deploy config

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate,
-  NavLink,
-} from "react-router-dom";
+import { Routes, Route, Navigate, NavLink, HashRouter } from "react-router-dom";
 import Products from "./components/Sheets/Products";
 import PricePlans from "./components/Sheets/PricePlans";
 import Pages from "./components/Sheets/Pages";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <nav>
         <ul className="menu menu-horizontal gap-2">
           <li>
             <NavLink
               to="/products"
-              className= {({ isActive }) =>
-                (isActive ? "menu-active" : "")
-              }
+              className={({ isActive }) => (isActive ? "menu-active" : "")}
             >
               Products
             </NavLink>
@@ -27,9 +19,7 @@ function App() {
           <li>
             <NavLink
               to="/price-plans"
-              className={({ isActive }) =>
-                (isActive ? "menu-active" : "")
-              }
+              className={({ isActive }) => (isActive ? "menu-active" : "")}
             >
               Price Plans
             </NavLink>
@@ -37,9 +27,7 @@ function App() {
           <li>
             <NavLink
               to="/pages"
-              className={({ isActive }) =>
-                (isActive ? "menu-active" : "")
-              }
+              className={({ isActive }) => (isActive ? "menu-active" : "")}
             >
               Pages
             </NavLink>
@@ -52,7 +40,7 @@ function App() {
         <Route path="/price-plans" element={<PricePlans />} />
         <Route path="/pages" element={<Pages />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
